7eaacbfb2c26635d124a30e5b6fd772ea6a1e0de
rabucejo <rogerhapay@gmail.com>
2023-04-04 16:36:59 +0800

    deleted home in navbar, logo will now redirect to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,11 +27,6 @@ const Navbar = () => {
           </button>
           <div class='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-              <li class='nav-item '>
-                <a class='nav-link active' aria-current='page' href='#./'>
-                  Home
-                </a>
-              </li>
               <li class='nav-item'>
                 <a class='nav-link active' aria-current='page' href='#./'>
                   Dashboard
