fd8abe7f26ef8d96df14e7aa9e349087308303f9
rabucejo <rogerhapay@gmail.com>
2023-04-04 08:45:19 +0800

    overriding styles for bs components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../styles/navbar.css'
 const Navbar = () => {
   return (
     <div>
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <div className='container-fluid'>
+        <div className='container-fluid '>
           <button
             class='navbar-toggler'
             type='button'
@@ -19,7 +20,7 @@ const Navbar = () => {
           </button>
           <div class='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-              <li class='nav-item'>
+              <li class='nav-item '>
                 <a class='nav-link active' aria-current='page' href='#./'>
                   Home
                 </a>
