e41bac08b6f18d19fd801b80e6c7fdbc6fb7efda
rabucejo <rogerhapay@gmail.com>
2023-04-05 09:39:36 +0800

    reset home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,9 @@
-import React, { useState } from 'react'
-import '../styles/home.css'
-import { Button, Modal } from 'react-bootstrap'
-const Home = () => {
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
-  return (
-    <div className='wrapper'>
-      <Button variant='primary' onClick={handleShow}>
-        Launch demo modal
-      </Button>
+import React from 'react'
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Modal body text goes here.</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant='primary' onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+const Home = () => {
+  return (
+    <div>
+      HOME
     </div>
   )
 }
