d0aff2c6e3248205a8ee2284d4e278b892ecf33c
rabucejo <rogerhapay@gmail.com>
2023-04-07 12:21:16 +0800

    deleted login modal

--- src/modals/Login.jsx
+++ src/modals/Login.jsx
@@ -3,39 +3,9 @@ import { Button, Modal, Form } from 'react-bootstrap'
 import '../styles/loginModal.css'
 const Login = ({ showModal, handleClose }) => {
   return (
-    <Modal
-      show={showModal}
-      onHide={handleClose}
-      className='modal-dialog modal-dialog-centered'
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>LOGIN MODAL</Modal.Title>
-      </Modal.Header>
-      <Modal.Body id='modalbody'>
-        <Form id='form'>
-          <Form.Group id='email' controlId='formBasicEmail'>
-            <Form.Control type='email' placeholder='Enter email' required />
-          </Form.Group>
-
-          <Form.Group id='password' controlId='formBasicPassword'>
-            <Form.Control type='password' placeholder='Password' required />
-          </Form.Group>
-          <Form.Group id='btn-form'>
-            <Button id='login' variant='primary' type='submit'>
-              Login
-            </Button>
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      {/* <Modal.Footer>
-        <Button variant='secondary' onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant='primary' onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer> */}
-    </Modal>
+  <>
+  {/* LOGIN MODAL */}
+  </>
   )
 }
 
