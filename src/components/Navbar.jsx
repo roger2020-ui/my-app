4e0d987b82439f92bb210c3a0e80de862561b531
rabucejo <rogerhapay@gmail.com>
2023-04-07 11:42:07 +0800

    deleted navbar contents dut to no idea for its ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from 'react'
 import '../styles/navbar.css'
-import img from '../img/dost.png'
-import { Container,Nav,Navbar } from 'react-bootstrap'
 const NavBar = () => {
   return (
     <div>
-      asdasd
+
     </div>
   )
 }
